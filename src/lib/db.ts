96e58f74e56d295ce8e2d60b695f8021af9551da
Gazi Faysal Jubayer <gazi.faysal.jubayer@gmail.com>
2025-12-31 14:15:06 +0600

    bug fix 1.002

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.DATABASE_URL;
-
-if (!MONGODB_URI) {
-    throw new Error(
-        "Please define the DATABASE_URL environment variable inside .env.local"
-    );
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -34,12 +26,20 @@ async function connectDB() {
         return cached.conn;
     }
 
+    const MONGODB_URI = process.env.DATABASE_URL;
+
+    if (!MONGODB_URI) {
+        throw new Error(
+            "Please define the DATABASE_URL environment variable inside .env.local"
+        );
+    }
+
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
         };
 
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
